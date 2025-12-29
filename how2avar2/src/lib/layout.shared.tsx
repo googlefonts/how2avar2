f8c1bb1a30bb2e317dd436736b888c7d4a53c7c3
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2025-12-29 14:52:20 -0500

    chore: remove links

--- how2avar2/src/lib/layout.shared.tsx
+++ how2avar2/src/lib/layout.shared.tsx
@@ -6,19 +6,5 @@ export function baseOptions(): BaseLayoutProps {
       title: "How2Avar2",
     },
     githubUrl: "https://github.com/googlefonts/how2avar2",
-    links: [
-      {
-        text: "Features",
-        url: "/docs/features/",
-        // secondary items will be displayed differently on navbar
-        secondary: false,
-      },
-      {
-        text: "Support",
-        url: "/docs/support/",
-        // secondary items will be displayed differently on navbar
-        secondary: false,
-      },
-    ],
   };
 }
