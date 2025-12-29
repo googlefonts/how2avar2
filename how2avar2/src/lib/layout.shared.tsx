445b51a11029b2c481314ad341dfedfd9cfcfabe
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2025-12-29 14:45:12 -0500

    feat: navigation links

--- how2avar2/src/lib/layout.shared.tsx
+++ how2avar2/src/lib/layout.shared.tsx
@@ -1,9 +1,24 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       title: "How2Avar2",
     },
+    githubUrl: "https://github.com/googlefonts/how2avar2",
+    links: [
+      {
+        text: "Features",
+        url: "/docs/features/",
+        // secondary items will be displayed differently on navbar
+        secondary: false,
+      },
+      {
+        text: "Support",
+        url: "/docs/support/",
+        // secondary items will be displayed differently on navbar
+        secondary: false,
+      },
+    ],
   };
 }
